b9eb29ec824e829e9bce4a114fff1a0ed96abe50
Gavin <gavinvm@protonmail.com>
2024-05-14 10:18:04 +0200

    Validate country

---
+++ src/country-validation.js
@@ -0,0 +1,33 @@
+import { COUNTRY_ADDRESS_POSTALS } from './country-list';
+
+export default function validateCountry() {
+  const countriesDatalist = document.getElementById('countries');
+
+  (function () {
+    COUNTRY_ADDRESS_POSTALS.forEach((country) => {
+      const option = document.createElement('option');
+      option.setAttribute('value', country.name);
+      countriesDatalist.appendChild(option);
+    });
+  })();
+
+  const country = document.getElementById('country');
+  const countryError = document.querySelector('#country + .error');
+
+  country.addEventListener('input', () => {
+    const found = COUNTRY_ADDRESS_POSTALS.some(
+      (countryDetails) => countryDetails.name === country.value,
+    );
+
+    if (country.validity.valueMissing) {
+      countryError.textContent = 'You need to enter a country.';
+    } else if (!found) {
+      countryError.textContent =
+        'Invalid country entered. Please check your spelling and try again.';
+      country.style.backgroundColor = '#fdd';
+    } else {
+      countryError.textContent = '';
+      country.style.backgroundColor = '#FFFFFF';
+    }
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './styles/styles.css';
 import validateEmail from './email-validation';
+import validateCountry from './country-validation';
 
 document.addEventListener('DOMContentLoaded', () => {
   validateEmail();
+  validateCountry();
 });
