11363f17d996c97a7b378886c582a144a6e0da8e
Gavin <gavinvm@protonmail.com>
2024-05-16 11:30:52 +0200

    Refactor zip code validation

--- src/zip-validation.js
+++ src/zip-validation.js
@@ -10,20 +10,31 @@ export default function validateZipCode(countrySelected) {
   );
   countryZipCodePattern = countryZipCodePattern[0].postal;
 
+  // If no postal key in country list
   if (!(countryZipCodePattern instanceof RegExp)) {
     countryZipCodePattern = /^[a-zA-Z0-9]{3,10}$/;
   }
 
   const zipCodeError = document.querySelector('#zip + .error');
+  let errorMessage = '';
+
+  const validateInput = function validateZipInput() {
+    if (zipCodeInput.classList.contains('default-state')) {
+      zipCodeInput.classList.remove('default-state');
+    }
+    zipCodeInput.setCustomValidity(errorMessage);
+    zipCodeError.textContent = errorMessage;
+  };
 
   const showError = function showZipCodeError() {
     if (zipCodeInput.validity.valueMissing) {
-      zipCodeError.textContent = 'You need to enter a zip code';
+      errorMessage = 'You need to enter a zip code';
     } else if (!countryZipCodePattern.test(zipCodeInput.value)) {
-      zipCodeError.textContent = 'You need to enter a valid zip code';
+      errorMessage = 'You need to enter a valid zip code';
     } else {
-      zipCodeError.textContent = '';
+      errorMessage = '';
     }
+    validateInput();
   };
 
   zipCodeInput.addEventListener('input', () => {
